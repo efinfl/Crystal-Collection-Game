edf17429d51cdf23f3f40341f08560270318e7d3
EF <ef@EFs-MacBook-Pro.local>
2018-11-10 18:40:10 -0500

    Initial commit

---
+++ assets/javasrcipt/game.js
@@ -0,0 +1,15 @@
+// Global Variables
+// Ids = variables
+// Data Types
+
+// Single Functions
+// 1. Computer chooses rand Num
+// 2. Assign Rand Values to Crystals 
+
+
+// Multi use Functions
+// 3. Sum crystals values when clicked
+// 4. Compare values to Rand Num Comp
+// 5. If Match User Wins append to DOM You Win, reset
+// 6. If No Match User loses , add loss, reset
+// 7. reset function
