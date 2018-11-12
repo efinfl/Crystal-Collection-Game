5cfe63804dd6d4a362d52f163ceb6662e677ee2c
EF <ef@EFs-MacBook-Pro.local>
2018-11-12 13:02:55 -0500

    random number selectors are working

--- assets/javasrcipt/game.js
+++ assets/javasrcipt/game.js
@@ -1,49 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-// Global Variables
-// Ids = variables
-var computerChoice = 0;
-var useSum = 0;
+    // Global Variables
+    // Ids = variables
+    var computerChoice = 0;
+    var useSum = 0;
+    // console.log("computer Choice " + computerChoice)
 
-var crystal_1 = "";
-var crystal_2 = "";
-var crystal_3 = "";
-var crystal_4 = "";
-
-
-// Data Types
-var winValue = 0;
-var lossesValue = 0;
-
-// Reset
-function reset() {
-    winValue = "";
-    lossesValue = ""
-}
-console.log ("is this working")
-
-// Single Functions
-// 1. Computer chooses rand Num
-function randomComputer() {
-    computerRando = Math.floor(Math.random() * 18) + 102;
-    console.log(computerRando);
-}
+    var crystal_1 = "";
+    var crystal_2 = "";
+    var crystal_3 = "";
+    var crystal_4 = "";
 
 
+    // Data Types
+    var winValue = 0;
+    var lossesValue = 0;
 
-// 2. Assign Rand Values to Crystals
-function crystalChoice() {
+    // Reset
+    function reset() {
+        winValue = "";
+        lossesValue = ""
+    }
+    console.log("is this working")
+
+    // Single Functions
+    // 1. Computer chooses rand Num
+
+    randomForComputer = Math.floor(Math.random() * 18) + 102;
+    console.log("computerChoic: " + randomForComputer);
+    
+    // 2. cristals get a random number
     crystalValue = Math.floor(Math.random() * 12);
     console.log("crystalValue: " + crystalValue);
-}
 
-
-// Multi use Functions
-// 3. Sum crystals values when clicked
-// 4. Compare values to Rand Num Comp
-// 5. If Match User Wins append to DOM You Win, reset
-// 6. If No Match User loses , add loss, reset
-// 7. reset function
+    // Multi use Functions
+    // 3. Sum crystals values when clicked
+    // 4. Compare values to Rand Num Comp
+    // 5. If Match User Wins append to DOM You Win, reset
+    // 6. If No Match User loses , add loss, reset
+    // 7. reset function
 
 });
