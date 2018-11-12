cf9f945ad9e7b33be74766334519222f3cff27aa
EF <ef@EFs-MacBook-Pro.local>
2018-11-12 08:15:29 -0500

    added global variables to js doc

--- assets/javasrcipt/game.js
+++ assets/javasrcipt/game.js
@@ -1,9 +1,32 @@
+$(document).ready(function() {
+
+})
+
 // Global Variables
 // Ids = variables
+var computerChoice = 0;
+var useSum = 0;
+
+var crystal_1 = "";
+var crystal_2 = "";
+var crystal_3 = "";
+var crystal_4 = "";
+
+var computerNumberRange = [];
+var usernumberRange = [];
+
 // Data Types
+var winValue = 0;
+var lossesValue = 0;
+
 
 // Single Functions
 // 1. Computer chooses rand Num
+function reset() {
+    winValue = "";
+    lossesValue = ""
+}
+
 // 2. Assign Rand Values to Crystals 
 
 
